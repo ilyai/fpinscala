c70f0257d8d8019fb0cbe90aa3c19441298255a1
Ilya Igonkin <osdev0@gmail.com>
2016-07-06 17:47:25 +0400

    Done exercise #8.1-8.4

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -14,14 +14,24 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+  def check: Either[(FailedCase,SuccessCount),SuccessCount]
+//  def &&(p: Prop): Prop = new Prop {
+//    def check = Prop.this.check && p.check
+//  }
 }
 
 object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
 object Gen {
   def unit[A](a: => A): Gen[A] = ???
+//  def listOf[A](a: Gen[A]): Gen[List[A]]
+//  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]]
+//  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+//    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive-start)))
 }
 
 trait Gen[A] {
@@ -29,7 +39,17 @@ trait Gen[A] {
   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
 }
 
+//case class Gen[A](sample: State[RNG, A])
+
 trait SGen[+A] {
 
 }
 
+object PropTest {
+  import Prop._
+  import Gen._
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
